0210b2aab6042d6c58dfbd956f06d76acffb0b7d
melik <melikcan.akc@gmail,com>
2022-06-29 18:24:16 +0100

    test

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
+#include "luxSensor.h"
+#include "ledMatrix.h"
+
+
+int main(void){
+
+    measureLux();
+    startup();
+
+    return 0;
+}
